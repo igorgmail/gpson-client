696a1d232431f291dcdcc74bb7ed544ce23edb4e
Igor.K <igor.k.nsk@gmail.com>
2024-01-19 23:29:48 +0700

    loginForm

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,11 +1,13 @@
 import { ComponentType } from 'react';
 import { Box } from '@mui/material';
+import { default as RegMain } from '../RegistrComponents/index';
 
 export const Main: ComponentType = () => {
 
   return (
     <Box display="flex" position="relative" flexDirection="row" height="100vh" width="100%">
 Main
+      <RegMain />
     </Box>
   );
 };
---
+++ src/components/RegistrComponents/index.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "./LoginForm"
+
+import './style.css'
+const index = () => {
+  return (
+    <LoginForm></LoginForm>
+  )
+}
+export default index
