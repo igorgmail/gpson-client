6f9c6b078c3975885d13cd78948bca41e39e9089
Igor.K <igor.k.nsk@gmail.com>
2024-01-23 18:29:55 +0700

    test new email input

--- src/components/RegistrComponents/Components/EmailInput.tsx
+++ src/components/RegistrComponents/Components/EmailInput.tsx
@@ -1,4 +1,4 @@
-import { TextField } from "@mui/material"
+import { Stack, TextField } from "@mui/material"
 import { FC } from "react"
 
 interface IEmailInput {
@@ -20,32 +20,65 @@ const EmailInput: FC<IEmailInput> = ({ value, emailError, changeHandler }) => {
   }
 
   return (
-    <TextField
+    <Stack display={'flex'} flexDirection={'column'} width={'100%'}>
+
+      <input
       onChange={changeHandler}
       value={value}
+        enterKeyHint='done'
       // required
-      error={emailError.error}
+        // error={emailError.error}
       name="email"
+        className={emailError.error ? `input-default input-error` : `input-default`}
       // id="outlined-start-adornment"
-      helperText={emailError.msg || 'e-mail'}
-      FormHelperTextProps={{
-        style: {
-          marginTop: '6px'
-        }
-      }}
+        // helperText={emailError.msg || 'e-mail'}
+        // FormHelperTextProps={{
+        //   style: {
+        //     marginTop: '6px'
+        //   }
+        // }}
 
-      // Применение стиля к input, а не ко всему TextField
-      inputProps={{
-        style: { padding: '6px' },
-        enterKeyHint: 'done'
-      }}
-      InputProps={{
-        style: {
-          ...inputDefaultStyle,
-          ...(emailError.error ? inputError : {})
-        },
-      }}
+        // // Применение стиля к input, а не ко всему TextField
+        // inputProps={{
+        //   style: { padding: '6px' },
+        //   enterKeyHint: 'done'
+        // }}
+        // InputProps={{
+        //   style: {
+        //     ...inputDefaultStyle,
+        //     ...(emailError.error ? inputError : {})
+        //   },
+        // }}
     />
+      <p className={emailError.error ? "helper-text--default helper-text--error" : `helper-text--default`}>{emailError.msg || 'e-mail'}</p>
+    </Stack>
+
+    // <TextField
+    //   onChange={changeHandler}
+    //   value={value}
+    //   // required
+    //   error={emailError.error}
+    //   name="email"
+    //   // id="outlined-start-adornment"
+    //   helperText={emailError.msg || 'e-mail'}
+    //   FormHelperTextProps={{
+    //     style: {
+    //       marginTop: '6px'
+    //     }
+    //   }}
+
+    //   // Применение стиля к input, а не ко всему TextField
+    //   inputProps={{
+    //     style: { padding: '6px' },
+    //     enterKeyHint: 'done'
+    //   }}
+    //   InputProps={{
+    //     style: {
+    //       ...inputDefaultStyle,
+    //       ...(emailError.error ? inputError : {})
+    //     },
+    //   }}
+    // />
   )
 }
 
